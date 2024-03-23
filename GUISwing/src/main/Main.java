35e33635c1e2c4b5fc23f3ba14f18f778ad62e11
Nikki Rivadeneira <lr.nrivadeneira@mmdc.mcl.edu.ph>
2024-03-23 14:06:21 +0800

    comment cleanup

--- GUISwing/src/main/Main.java
+++ GUISwing/src/main/Main.java
@@ -10,22 +10,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		/*
-		//JFrame = a GUI window to add components to
-		JFrame frame = new JFrame(); //create a frame
-		frame.setTitle("GCC"); //sets title of frame
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application when x button is clicked
-		frame.setResizable(false); //prevent frame from being resized
-		frame.setSize(420,420); //sets the x-dimension, and y-dimension of frame
-		frame.setVisible(true); //make frame visible
-		
-		ImageIcon imageLogo = new ImageIcon("./src/logo.png"); //create an ImageIcon
-		frame.setIconImage(imageLogo.getImage()); //change icon of frame
-//		frame.getContentPane().setBackground(Color.cyan); //change color of background,pick from predetermined color
-		frame.getContentPane().setBackground(new Color(224, 255, 255)); //change color of background using rgb code
-		*/
-
-		MyFrame myFrame = new MyFrame();
+		//MyFrame myFrame = new MyFrame(); // call the MyFrame class
+		new MyFrame(); //use this syntax if you will not use the declared variable like the code above
 	}
 
 }
