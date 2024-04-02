6db3821d6df8c7caf6638d68d7323b16476fdab0
Nikki Rivadeneira <lr.nrivadeneira@mmdc.mcl.edu.ph>
2024-04-02 17:58:15 +0800

    Created myButton under MyLaunchPage class to call MyNewWindow

--- GUISwing/src/main/Main.java
+++ GUISwing/src/main/Main.java
@@ -4,8 +4,8 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-
-
+		
+		
 		
 		
 	}
---
+++ GUISwing/src/main/MyNewWindow.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MyNewWindow {
+	
+	JFrame frame = new JFrame();
+	JLabel label = new JLabel("Hello!");
+	
+	MyNewWindow(){
+		
+		label.setBounds(0, 0, 100, 50);
+		label.setFont(new Font(null,Font.PLAIN,25));
+		
+		frame.add(label);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+
+}
